412dce182177834be0ac12f50247e58b9980202c
dheeraj <dheeraj.pure.tech@gmail.com>
2017-05-17 17:45:12 +0530

    initial code

---
+++ src/dp/java8/feature/lambda/_01/BasicLambda.java
@@ -0,0 +1,20 @@
+package dp.java8.feature.lambda._01;
+
+public class BasicLambda {
+	  public static void main(String args[])
+	    {
+	        // lambda expression to implement above
+	        // functional interface. This interface
+	        // by default implements abstractFun()
+	        FuncInterface fobj = (int x, int y)->System.out.println(x+y);
+	 
+	        // This calls above lambda expression and prints 10.
+	        fobj.abstractFun(5,10); 
+	    }
+}
+
+interface FuncInterface
+{
+    // An abstract function
+    void abstractFun(int x,int y);
+}
