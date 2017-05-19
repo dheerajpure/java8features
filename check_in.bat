e0ad65bcca1e17813c80a4031f4a081cd819db97
Dheeraj Pure <dheeraj_pure@syntelinc.com>
2017-05-19 16:42:51 +0530

    code modified

---
+++ check_in.bat
@@ -0,0 +1,8 @@
+@rem git config --global http.proxy http[s]://userName:password@proxyaddress:port
+@rem git config --global http.proxy 127.0.0.1:8087
+
+git config --global http.proxy 10.119.32.71:8080
+
+git add * 
+git commit -m "code modified"
+git push
---
+++ check_out.bat
@@ -0,0 +1,6 @@
+@rem git config --global http.proxy http[s]://userName:password@proxyaddress:port
+@rem git config --global http.proxy 127.0.0.1:8087
+
+git config --global http.proxy 10.119.32.71:8080
+
+git clone https://github.com/dheerajpure/java8features.git
